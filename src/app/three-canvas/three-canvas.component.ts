48fd861428816cdb61ee90e703d88ca4e7fd2f32
Mary Kabulu <mkabulu@gmail.com>
2025-03-19 11:33:10 +0200

    initial commit

---
+++ src/app/three-canvas/three-canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './three-canvas.component.html',
+  styleUrl: './three-canvas.component.css'
+})
+export class ThreeCanvasComponent {
+
+}
